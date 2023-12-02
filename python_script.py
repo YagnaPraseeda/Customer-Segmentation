3ba9d8f6ce6be74047cf78f4523d98d1b8a3561b
YagnaPraseeda <151584865+YagnaPraseeda@users.noreply.github.com>
2023-12-02 13:17:19 -0500

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -5,19 +5,22 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 def preprocess_data(file_path):
-    
+    # Loading the dataset
     df = pd.read_csv(file_path)
-
+    
+    # Preprocessing the data
     df = df.drop_duplicates()
     df['Total_Purchase'] = df['Quantity'] * df['UnitPrice']
-
+    
+    # Removing outliers based on Total_Purchase
     Q1_total = df['Total_Purchase'].quantile(0.25)
     Q3_total = df['Total_Purchase'].quantile(0.75)
     IQR_total = Q3_total - Q1_total
     lower_bound_total = Q1_total - 1.5 * IQR_total
     upper_bound_total = Q3_total + 1.5 * IQR_total
     df = df[(df['Total_Purchase'] >= lower_bound_total) & (df['Total_Purchase'] <= upper_bound_total)]
-
+    
+    # Removing outliers based on Quantity
     Q1_quantity = df['Quantity'].quantile(0.25)
     Q3_quantity = df['Quantity'].quantile(0.75)
     IQR_quantity = Q3_quantity - Q1_quantity
@@ -25,6 +28,7 @@ def preprocess_data(file_path):
     upper_bound_quantity = Q3_quantity + 1.5 * IQR_quantity
     df = df[(df['Quantity'] >= lower_bound_quantity) & (df['Quantity'] <= upper_bound_quantity)]
 
+    # Removing outliers based on UnitPrice
     Q1_unit_price = df['UnitPrice'].quantile(0.25)
     Q3_unit_price = df['UnitPrice'].quantile(0.75)
     IQR_unit_price = Q3_unit_price - Q1_unit_price
@@ -32,17 +36,21 @@ def preprocess_data(file_path):
     upper_bound_unit_price = Q3_unit_price + 1.5 * IQR_unit_price
     df = df[(df['UnitPrice'] >= lower_bound_unit_price) & (df['UnitPrice'] <= upper_bound_unit_price)]
 
+    #Filtering based on CustomerID
     df = df[(df['CustomerID'] >= 12000) & (df['CustomerID'] <= 18500)]
 
     return df
 
 def perform_customer_clustering(df):
-    
+
+    # Selecting relevant columns for clustering
     customer_data = df[['CustomerID', 'Total_Purchase']].dropna()
 
+    # Standardizing the features
     scaler = StandardScaler()
     customer_data_scaled = scaler.fit_transform(customer_data[['Total_Purchase']])
 
+    # Determining the optimal number of clusters (Elbow method)
     distortions = []
     k_range = range(1, 11)
     for k in k_range:
@@ -50,6 +58,7 @@ def perform_customer_clustering(df):
         kmeans.fit(customer_data_scaled)
         distortions.append(kmeans.inertia_)
 
+    # Plotting the elbow graph
     plt.figure(figsize=(10, 6))
     plt.plot(k_range, distortions, marker='o')
     plt.title('Elbow Method for Optimal k (Customer Clustering)')
@@ -57,20 +66,27 @@ def perform_customer_clustering(df):
     plt.ylabel('Distortion (Within-cluster Sum of Squares)')
     plt.show()
 
+    # Choosing the optimal k value
     optimal_k = 3
 
+    # Applying KMeans clustering with the optimal k
     kmeans = KMeans(n_clusters=optimal_k, random_state=42)
     customer_data['Cluster'] = kmeans.fit_predict(customer_data_scaled)
 
+    # Getting cluster centers in the scaled space and inverse transform to original scale
     scaled_cluster_centers = kmeans.cluster_centers_
     centers_original_scale = scaler.inverse_transform(scaled_cluster_centers)
 
+    # Plotting the clusters and cluster centers
     plt.figure(figsize=(15, 8))
     plt.scatter(customer_data['CustomerID'], customer_data['Total_Purchase'], c=customer_data['Cluster'], cmap='viridis')
 
+    # Annotating the cluster centers
     for i, center_scaled in enumerate(scaled_cluster_centers):
+        # Inverse transform the center's Total_Purchase value
         center_val = centers_original_scale[i, 0]
 
+        # Calculating the average CustomerID for the cluster for plotting
         mean_customer_id = customer_data[customer_data['Cluster'] == i]['CustomerID'].mean()
 
         plt.scatter(mean_customer_id, center_val, c='black', marker='o', s=200)
@@ -83,14 +99,17 @@ def perform_customer_clustering(df):
     plt.show()
 
 def perform_geographical_clustering(df):
-
+    # Selecting relevant columns for clustering
     geo_data = df[['Country', 'Total_Purchase']].dropna()
 
+    # Converting 'Country' to numerical values using label encoding
     geo_data['Country'] = geo_data['Country'].astype('category').cat.codes
 
+    # Standardizing the features
     scaler = StandardScaler()
     geo_data_scaled = scaler.fit_transform(geo_data[['Total_Purchase']])
 
+    # Determining the optimal number of clusters
     distortions = []
     k_range = range(1, 11)
     for k in k_range:
@@ -98,6 +117,7 @@ def perform_geographical_clustering(df):
         kmeans.fit(geo_data_scaled)
         distortions.append(kmeans.inertia_)
 
+    # Plotting the elbow graph
     plt.figure(figsize=(10, 6))
     plt.plot(k_range, distortions, marker='o')
     plt.title('Elbow Method for Optimal k (Geographical Clustering)')
@@ -105,14 +125,18 @@ def perform_geographical_clustering(df):
     plt.ylabel('Distortion (Within-cluster Sum of Squares)')
     plt.show()
 
+    # Choosing the optimal k value
     optimal_k = 3
 
+    # Applying KMeans clustering with the optimal k
     kmeans = KMeans(n_clusters=optimal_k, random_state=42)
     geo_data['Cluster'] = kmeans.fit_predict(geo_data_scaled)
 
+    # Getting cluster centers in the scaled space and inverse transform to original scale
     scaled_cluster_centers = kmeans.cluster_centers_
     centers_original_scale = scaler.inverse_transform(scaled_cluster_centers)
 
+    # Plotting the clusters and cluster centers
     plt.figure(figsize=(15, 8))
     plt.scatter(geo_data['Country'], geo_data['Total_Purchase'], c=geo_data['Cluster'], cmap='viridis')
 
@@ -126,12 +150,14 @@ def perform_geographical_clustering(df):
     plt.show()
 
 def perform_product_clustering(df):
-
+    # Selecting relevant columns for clustering
     product_data = df[['Quantity', 'UnitPrice']].dropna()
 
+    # Standardizing the features
     scaler = StandardScaler()
     product_data_scaled = scaler.fit_transform(product_data)
 
+    # Determining the optimal number of clusters (elbow method)
     distortions = []
     k_range = range(1, 11)
     for k in k_range:
@@ -139,6 +165,7 @@ def perform_product_clustering(df):
         kmeans.fit(product_data_scaled)
         distortions.append(kmeans.inertia_)
 
+    # Plotting the elbow graph
     plt.figure(figsize=(10, 6))
     plt.plot(k_range, distortions, marker='o')
     plt.title('Elbow Method for Optimal k (Product Clustering)')
@@ -146,17 +173,22 @@ def perform_product_clustering(df):
     plt.ylabel('Distortion (Within-cluster Sum of Squares)')
     plt.show()
 
+    # Choosing the optimal k value
     optimal_k = 3
 
+    # Applying KMeans clustering with the optimal k
     kmeans = KMeans(n_clusters=optimal_k, random_state=42)
     product_data['Cluster'] = kmeans.fit_predict(product_data_scaled)
 
+    # Getting cluster centers in the scaled space and inverse transform to original scale
     scaled_cluster_centers = kmeans.cluster_centers_
     centers_original_scale = scaler.inverse_transform(scaled_cluster_centers)
 
+    # Plotting the clusters and cluster centers
     plt.figure(figsize=(15, 8))
     plt.scatter(product_data['Quantity'], product_data['UnitPrice'], c=product_data['Cluster'], cmap='viridis')
 
+    # Annotating the cluster centers
     for i, center_scaled in enumerate(scaled_cluster_centers):
         
         center_quantity, center_price = centers_original_scale[i, :]
@@ -171,27 +203,27 @@ def perform_product_clustering(df):
     plt.show()
 
 def calculate_rfm(data):
-    
+    # Calculating Recency, Frequency, and Monetary values for each customer
     rfm_data = data.groupby('CustomerID').agg({
         'InvoiceDate': lambda x: (pd.to_datetime('now') - pd.to_datetime(x.max())).days,
         'InvoiceNo': 'nunique',
         'Total_Purchase': 'sum'
     }).reset_index()
 
-    
+    # Renaming columns
     rfm_data.columns = ['CustomerID', 'Recency', 'Frequency', 'Monetary']
 
     return rfm_data
 
 def perform_rfm_clustering(data):
-    
+    # Selecting relevant columns for clustering
     rfm_data = data[['Recency', 'Frequency', 'Monetary']].dropna()
 
-    
+    # Standardizing the features
     scaler = StandardScaler()
     rfm_data_scaled = scaler.fit_transform(rfm_data)
 
-    
+    # Determining the optimal number of clusters (elbow method)
     distortions = []
     k_range = range(1, 11)
     for k in k_range:
@@ -199,7 +231,7 @@ def perform_rfm_clustering(data):
         kmeans.fit(rfm_data_scaled)
         distortions.append(kmeans.inertia_)
 
-    
+    # Plotting the elbow graph
     plt.figure(figsize=(10, 6))
     plt.plot(k_range, distortions, marker='o')
     plt.title('Elbow Method for Optimal k (RFM Clustering)')
@@ -207,12 +239,14 @@ def perform_rfm_clustering(data):
     plt.ylabel('Distortion (Within-cluster Sum of Squares)')
     plt.show()
 
-    
+    # Choosing the optimal k value
     optimal_k = 3
 
+    # Applying KMeans clustering with the optimal k
     kmeans = KMeans(n_clusters=optimal_k, random_state=42)
     rfm_data['Cluster'] = kmeans.fit_predict(rfm_data_scaled)
 
+    # Plotting the clusters    
     plt.figure(figsize=(15, 8))
     plt.scatter(rfm_data['Recency'], rfm_data['Monetary'], c=rfm_data['Cluster'], cmap='viridis')
     plt.title('RFM Clustering')
@@ -220,6 +254,7 @@ def perform_rfm_clustering(data):
     plt.ylabel('Monetary')
     plt.show()
 
+# Loading and preprocessing data
 file_path = "C:/Users/user/Informatics/Project/praseeda project/dataset.csv"
 df = preprocess_data(file_path)
 
